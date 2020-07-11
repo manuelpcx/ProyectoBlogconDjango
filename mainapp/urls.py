abe31821f5fb956dbebc6daa53b48f35712b847a
manuelpcx <jesus.medina05@gmail.com>
2020-07-11 02:47:55 -0400

    n

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('inicio/', views.index, name='inicio'),
     path('registro/', views.register_page, name='register'),
     path('login/', views.login_page, name='login'),
